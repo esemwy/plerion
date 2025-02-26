35bcd718e18dc3a43c87583889c26535db52ceee
esemwy <github.5.smy@neverbox.com>
2025-02-26 15:46:00 -0500

    shebang

--- _build/build.sh
+++ _build/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mkdir html
 for i in ../[0]*.md ; do $(pandoc -f gfm -t html -o html/$(basename ${i} .md).html $i) ; done
 pandoc -t html -o pdf/plerion_ashcan.pdf -s html/*.html --toc --toc-depth=1 --css="css/plerion.css" --metadata-file=metadata.yml --template=template/template.html --pdf-engine=weasyprint
